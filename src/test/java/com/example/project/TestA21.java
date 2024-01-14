ea70636001232468f7bdca90b9503f792a7b7177
Kyu Lee <kwooler@gmail.com>
2024-01-14 14:06:21 -0800

    A21

--- src/test/java/com/example/project/TestA21.java
+++ src/test/java/com/example/project/TestA21.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.*;
 import java.util.*;
+// import java.util.ArrayList;
 
 public class TestA21 {
 
@@ -40,6 +41,61 @@ public class TestA21 {
     // Test 2
     A21.getRdnum(num);
     Arrays.sort(num);
+    ans = num[1];
+
+    System.out.println("Random numbers " + num[0] + " " + num[1] + " " + num[2]);
+    System.out.println("Answer " + ans);
+    result = A21.getMed(num);
+
+    // assertion
+    // assertEquals("Hello world!\n", bos.toString());
+    assertEquals(result, ans);
+    // undo the binding in System
+    // System.setOut(originalOut);
+  }
+
+  @Test
+  public void testA22() {
+    /* System output redirection */
+    // PrintStream originalOut = System.out;
+    // ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    // System.setOut(new PrintStream(bos));
+
+    /* For Test */
+    int result;
+    int[] num = { 15, 10, 20 };
+    // action
+    // Test 1
+    int ans = 15;
+
+    System.out.println("Random numbers " + num[0] + " " + num[1] + " " + num[2]);
+    System.out.println("Answer " + ans);
+    result = A21.getMed(num);
+
+    // assertion
+    // assertEquals("Hello world!\n", bos.toString());
+    assertEquals(result, ans);
+
+    // Test 2
+    num[0] = 20;
+    num[1] = 10;
+    num[2] = 15;
+    ans = num[2];
+
+    System.out.println("Random numbers " + num[0] + " " + num[1] + " " + num[2]);
+    System.out.println("Answer " + ans);
+    result = A21.getMed(num);
+
+    // assertion
+    // assertEquals("Hello world!\n", bos.toString());
+    assertEquals(result, ans);
+    // undo the binding in System
+    // System.setOut(originalOut);
+
+    // Test 3
+    num[0] = 20;
+    num[1] = 15;
+    num[2] = 10;
     ans = num[1];
 
     System.out.println("Random numbers " + num[0] + " " + num[1] + " " + num[2]);
